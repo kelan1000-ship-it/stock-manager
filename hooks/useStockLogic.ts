41892c4b854eae099276c3e004ace150b220bcf0
Kelan <kelan1000@gmail.com>
2026-04-09 00:33:55 +0100

    fixed console error

--- hooks/useStockLogic.ts
+++ hooks/useStockLogic.ts
@@ -66,7 +66,8 @@ export function useStockLogic() {
     toggleArchive,
     restoreProduct,
     handleDeleteProduct,
-    bulkUpdateProducts
+    bulkUpdateProducts,
+    confirmStockCheck
   } = useProductOperations(
     currentBranch,
     setBranchData,
